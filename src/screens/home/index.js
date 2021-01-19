206bc03532d3069948f32dda4ccb9b0d41e8c72e
usamatcheema <usamatcheema@gmail.com>
2021-01-19 15:51:41 -0500

    displaying home screen

---
+++ src/screens/home/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, ImageBackground} from 'react-native';
+import styles from './styles'
+
+const HomeScreen = (props) => {
+  return (
+    <View>
+      {/* Search Bar */}
+      <ImageBackground 
+      source={require('../../../assets/images/wallpaper.jpg')} 
+      style={styles.image}>
+      {/* Title */}
+
+      {/* Button */}
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default HomeScreen;
