4eaedca9040fa909c7465215a647ce5b4f44caa2
usamatcheema <usamatcheema@gmail.com>
2021-01-21 12:13:16 -0500

    formatting changes

---
+++ src/components/lodgingDetails/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const LodgingDetails = (props) => {
+  return (
+    <View>
+      <Text>Checking LodgingDetails</Text>
+    </View>
+  );
+}
+
+export default LodgingDetails;
