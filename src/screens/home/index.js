80582c8d0034b8186cee9520a11d70d8024e41f0
usamatcheema <usamatcheema@gmail.com>
2021-01-21 11:06:21 -0500

    home screen styling added

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,17 +1,30 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import {View, Text, ImageBackground} from 'react-native';
-import styles from './styles'
+import {View, Text, ImageBackground, Pressable} from 'react-native';
+import styles from './styles';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 
 const HomeScreen = (props) => {
   return (
     <View>
-      {/* Search Bar */}
-      <ImageBackground 
-      source={require('../../../assets/images/wallpaper.jpg')} 
-      style={styles.image}>
-      {/* Title */}
+      <ImageBackground
+        source={require('../../../assets/images/wallpaper.jpg')}
+        style={styles.image}>
 
-      {/* Button */}
+      <Pressable
+        style={styles.searchBarButton}
+        onPress={() => console.warn('Explore Btn clicked')}>
+        <Fontisto name="search" size={25} color={"#f15454"} />
+        <Text style={styles.searchBarButtonText}>Where are you headed?</Text>
+      </Pressable>
+
+      <Text style={styles.title}>Go Near</Text>
+
+      <Pressable
+        style={styles.button}
+        onPress={() => console.warn('Explore Btn clicked')}>
+        <Text style={styles.buttonText}>Explore nearby places</Text>
+      </Pressable>
       </ImageBackground>
     </View>
   );
--- src/screens/home/styles.js
+++ src/screens/home/styles.js
@@ -1,10 +1,53 @@
-import {StyleSheet} from 'react-native'
+/* eslint-disable prettier/prettier */
+import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    image: {
-        width: '100%',
-        height: 710,
-    }
+  image: {
+    width: '100%',
+    height: 500,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 100,
+    fontWeight: 'bold',
+    color: 'white',
+    width: '70%',
+    marginLeft: 20,
+  },
+  button: {
+      backgroundColor: 'white',
+      marginTop: 10,
+      marginLeft: 20,
+      width: 200,
+      height: 30,
+      borderRadius: 50,
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  buttonText: {
+      color: 'black',
+      fontWeight: 'bold',
+      fontSize: 16,
+  },
+  searchBarButton: {
+      backgroundColor: 'white',
+      width: '100%',
+      height: 30,
+      borderRadius: 50,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      position: 'absolute',
+      top: 50,
+      zIndex: 100,
+  },
+  searchBarButtonText: {
+      color: 'black',
+      fontWeight: 'bold',
+      fontSize: 16,
+  },
+
 });
 
-export default styles
+export default styles;
