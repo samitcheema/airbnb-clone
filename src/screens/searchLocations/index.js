b36503ccb8f5f2e098cfee66339c60c07a964238
usamatcheema <usamatcheema@gmail.com>
2021-01-22 13:43:55 -0500

    implemented search locations screen

--- src/screens/searchLocations/index.js
+++ src/screens/searchLocations/index.js
@@ -1,11 +1,32 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, FlatList } from "react-native";
 import styles from "./styles";
+import searchResults from "../../../assets/data/searchData";
+import Entypo from "react-native-vector-icons/Entypo";
 
 const SearchLocationsScreen = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <View>
-      <Text>Am I working?</Text>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Where are you headed?"
+        value={searchTerm}
+        onChangeText={setSearchTerm}
+      />
+
+      <FlatList
+        data={searchResults}
+        renderItem={({ item }) => (
+          <View style={styles.rowContainer}>
+            <View style={styles.iconContainer}>
+              <Entypo name={"location-pin"} size={30} />
+            </View>
+            <Text style={styles.locationsText}>{item.description}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
--- src/screens/searchLocations/styles.js
+++ src/screens/searchLocations/styles.js
@@ -1,5 +1,27 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    margin: 20,
+  },
+
+  searchInput: {
+    fontSize: 18,
+  },
+
+  rowContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "lightgrey",
+  },
+  iconContainer: {
+    backgroundColor: "lightgrey",
+    borderRadius: 10,
+    margin: 10,
+  },
+  locationsText: {},
+});
 
 export default styles;
