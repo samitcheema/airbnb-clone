be3079ca3f5f92d98451df4c94ee1018a457d22f
usamatcheema <usamatcheema@gmail.com>
2021-01-23 01:17:09 -0500

    added bottom tab navigation

---
+++ src/navigation/homeBottomTabNavigation.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Fontisto from "react-native-vector-icons/Fontisto";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import Feather from "react-native-vector-icons/Feather";
+import EvilIcons from "react-native-vector-icons/EvilIcons";
+
+import HomeScreen from "./../screens/home/index";
+import GuestsScreen from "./../screens/guests/index";
+import SearchLocationsScreen from "./../screens/searchLocations/index";
+import SearchResultsScreen from "./../screens/searchResults/index";
+
+const Tab = createBottomTabNavigator();
+
+const HomeBottomTabNavigation = () => {
+  return (
+    <Tab.Navigator tabBarOptions={{ activeTintColor: "#f15454" }}>
+      <Tab.Screen
+        name="Airbnb"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="airbnb" size={25} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Messages"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Feather name="message-square" size={25} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Saved"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Fontisto name="heart-alt" size={25} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <EvilIcons name="user" size={25} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default HomeBottomTabNavigation;
