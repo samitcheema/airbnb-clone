d46d85e3d5b38b6c80a07b6d03a5edbff86db03a
usamatcheema <usamatcheema@gmail.com>
2021-01-23 01:17:26 -0500

    routing

---
+++ src/navigation/router.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import HomeBottomTabNavigation from "./homeBottomTabNavigation";
+import HomeScreen from "../screens/home";
+import SearchLocationsScreen from "./../screens/searchLocations/index";
+import SearchResultsScreen from "./../screens/searchResults/index";
+import GuestsScreen from "./../screens/guests/index";
+
+const Stack = createStackNavigator();
+
+const Router = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeBottomTabNavigation}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Search Locations"
+          component={SearchLocationsScreen}
+          options={{ headerTitleAlign: "center", title: "Search" }}
+        />
+        <Stack.Screen
+          name="Search Results"
+          component={SearchResultsScreen}
+          options={{ headerTitleAlign: "center", title: "Results" }}
+        />
+        <Stack.Screen
+          name="Guests"
+          component={GuestsScreen}
+          options={{ headerTitleAlign: "center", title: "Guests" }}
+        /> 
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Router;
