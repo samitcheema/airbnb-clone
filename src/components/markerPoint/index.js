974e0c70bdc4748a20f029281991252a91ce2328
usamatcheema <usamatcheema@gmail.com>
2021-01-25 22:34:58 -0500

    marker component

---
+++ src/components/markerPoint/index.js
@@ -0,0 +1,28 @@
+import React, { useStat } from "react";
+import { View, Text } from "react-native";
+import { Marker } from "react-native-maps";
+import lodgingData from "../../../assets/data/lodgingData";
+
+const MarkerPoint = (props) => {
+  const { coordinate, totalPrice, onPress, isSelected } = props;
+  return (
+    <View>
+      <Marker coordinate={coordinate}>
+        <Text
+          style={{
+            backgroundColor: "white",
+            color: "black",
+            padding: 5,
+            borderRadius: 15,
+            borderWidth: 1,
+            fontWeight: "bold",
+          }}
+        >
+          ${totalPrice}
+        </Text>
+      </Marker>
+    </View>
+  );
+};
+
+export default MarkerPoint;
